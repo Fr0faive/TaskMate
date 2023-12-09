06ffc9c256fc29fe3e8a07a4cd45dbd1029c3607
fr0faive <faikar.moch.tajudin@gmail.com>
2023-12-09 07:19:08 +0700

    feat: menu login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.lastpro.taskmate"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.lastpro.taskmate"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
